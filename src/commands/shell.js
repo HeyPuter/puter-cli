1d028659491ab782c027265c88382f1e465251d0
Ibrahim H <bitsnaps@yahoo.fr>
2025-01-22 11:38:32 +0100

    feat: improve login ux

--- src/commands/shell.js
+++ src/commands/shell.js
@@ -2,7 +2,7 @@ import readline from 'node:readline';
 import chalk from 'chalk';
 import Conf from 'conf';
 import { execCommand, getPrompt } from '../executor.js';
-import { getAuthToken } from './auth.js';
+import { getAuthToken, login } from './auth.js';
 import { PROJECT_NAME } from '../commons.js';
 
 const config = new Conf({ projectName: PROJECT_NAME });
@@ -24,16 +24,17 @@ export function updatePrompt(currentPath) {
 /**
  * Start the interactive shell
  */
-export function startShell() {
+export async function startShell() {
   if (!getAuthToken()) {
-    console.log(chalk.red('Please login first using: puter login'));
-    process.exit(1);
+    console.log(chalk.cyan('Please login first (or use CTRL+C to exit):'));
+    await login();
+    console.log(chalk.green(`Now just type: ${chalk.cyan('puter')} to begin.`));
+    process.exit(0);
   }
-
-  rl.setPrompt(getPrompt());
 
   try {
     console.log(chalk.green('Welcome to Puter-CLI! Type "help" for available commands.'));
+    rl.setPrompt(getPrompt());
     rl.prompt();
 
     rl.on('line', async (line) => {

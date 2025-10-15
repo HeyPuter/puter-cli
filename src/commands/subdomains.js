120612b53e68f9ba6103695b12b46a30997f5dfb
Reynaldi Chernando <reynaldichernando@gmail.com>
2025-10-15 17:26:35 +0700

    Move sites and subdomains to use puterjs

--- src/commands/subdomains.js
+++ src/commands/subdomains.js
@@ -9,20 +9,16 @@ import { getPuter } from '../modules/PuterModule.js';
  * @returns {Array} - Array of subdomains.
  */
 export async function getSubdomains(args = {}) {
-    const response = await fetch(`${API_BASE}/drivers/call`, {
-        method: 'POST',
-        headers: getHeaders(),
-        body: JSON.stringify({
-            interface: 'puter-subdomains',
-            method: 'select',
-            args: args
-        })
-    });
+    const puter = getPuter();
+    let result;
 
-    if (!response.ok) {
-        throw new Error('Failed to fetch subdomains.');
+    try {
+        result = await puter.hosting.list();    
+    } catch (error) {
+        console.log(chalk.red(`Error when getting subdomains.\nError: ${error?.message}`));
     }
-    return await response.json();
+
+    return result;
 }
 
 /**
@@ -35,32 +31,22 @@ export async function deleteSubdomain(args = []) {
         console.log(chalk.red('Usage: domain:delete <subdomain_id>'));
         return false;
     }
+    const puter = getPuter();
     const subdomains = args;
     for (const subdomain of subdomains)
         try {
-        const response = await fetch(`${API_BASE}/drivers/call`, {
-            headers: getHeaders(),
-            method: 'POST',
-            body: JSON.stringify({
-            interface: 'puter-subdomains',
-            method: 'delete',
-            args: {
-                id: { subdomain: subdomain }
-            }
-            })
-        });
+        const success = await puter.hosting.delete(subdomain);
     
-        const data = await response.json();
-        if (!data.success) {
-            if (data.error?.code === 'entity_not_found') {
-                console.log(chalk.red(`Subdomain: "${subdomain}" not found`));
-                return false;
-            }
+        if (!success) {
             console.log(chalk.red(`Failed to delete subdomain: ${data.error?.message}`));
             return false;
         }
         console.log(chalk.green('Subdomain deleted successfully'));
         } catch (error) {
+            if (error.error?.code === 'entity_not_found') {
+                console.log(chalk.red(`Subdomain: "${subdomain}" not found`));
+                return false;
+            }
             console.error(chalk.red('Error deleting subdomain:'), error.message);
         }
         return true;
@@ -73,35 +59,19 @@ export async function deleteSubdomain(args = []) {
  * @returns {Object} - Hosting details (e.g., subdomain).
  */
 export async function createSubdomain(subdomain, remoteDir) {
-    const response = await fetch(`${API_BASE}/drivers/call`, {
-        method: 'POST',
-        headers: getHeaders(),
-        body: JSON.stringify({
-            interface: 'puter-subdomains',
-            method: 'create',
-            args: {
-                object: {
-                    subdomain: subdomain,
-                    root_dir: remoteDir
-                }
-            }
-        })
-    });
+    const puter = getPuter();
+    let result;
 
-    if (!response.ok) {
-        throw new Error('Failed to host directory.');
-    }
-    const data = await response.json();
-    if (!data.success || !data.result) {
-        if (data.error?.code === 'already_in_use') {
-            // data.error?.status===409
-            console.log(chalk.yellow(`Subdomain already taken!\nMessage: ${data?.error?.message}`));
+    try {
+        result = await puter.hosting.create(subdomain, remoteDir);
+    } catch (error) {
+        if (error?.error?.code === 'already_in_use') {
+            console.log(chalk.yellow(`Subdomain already taken!\nMessage: ${error?.error?.message}`));
             return false;
         }
-        console.log(chalk.red(`Error when creating "${subdomain}".\nError: ${data?.error?.message}\nCode: ${data.error?.code}`));
-        return false;
+        console.log(chalk.red(`Error when creating "${subdomain}".\nError: ${error?.error?.message}\nCode: ${error?.error?.code}`));
     }
-    return data.result;
+    return result;
 }
 
 /**

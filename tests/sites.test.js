e4888b90c25b947f022bb92c2985727f956406fd
Reynaldi Chernando <reynaldichernando@gmail.com>
2025-11-12 14:44:31 +0700

    Add info site test

--- tests/sites.test.js
+++ tests/sites.test.js
@@ -1,15 +1,17 @@
 import { describe, vi, expect, it } from "vitest";
-import { listSites } from "../src/commands/sites";
+import { infoSite, listSites } from "../src/commands/sites";
 import { getSubdomains } from "../src/commands/subdomains";
+import { getPuter } from "../src/modules/PuterModule.js";
 
 vi.mock("../src/commands/subdomains")
+vi.mock("../src/modules/PuterModule")
 vi.spyOn(console, "log").mockImplementation(() => { });
 
 describe("listSites", () => {
   it("should list sites successfully", async () => {
     vi.mocked(getSubdomains).mockResolvedValue([{
       uid: "123",
-      subdomain: "hehe",
+      subdomain: "hehe.puter.site",
       root_dir: { path: "/some/path" },
       created_at: new Date().toISOString(),
       protected: false
@@ -17,5 +19,27 @@ describe("listSites", () => {
     await listSites();
     expect(getSubdomains).toHaveBeenCalled();
     expect(console.log).toHaveBeenCalledWith(expect.stringContaining("Total Sites: 1"))
+  })
+})
+
+describe("infoSite", () => {
+  it("should get site info successfully", async () => {
+    const mockHostingGet = vi.fn().mockResolvedValue({
+      uid: "123",
+      subdomain: "hehe.puter.site",
+      root_dir: { path: "/some/path" },
+      created_at: new Date().toISOString(),
+      protected: false
+    });
+    vi.mocked(getPuter).mockReturnValue({
+      hosting: {
+        get: mockHostingGet
+      }
+    })
+
+    await infoSite("hehe.puter.site")
+    expect(getPuter).toHaveBeenCalled();
+    expect(mockHostingGet).toHaveBeenCalled();
+    expect(console.log).toHaveBeenCalledWith(expect.stringContaining("hehe.puter.site"))
   })
 })
